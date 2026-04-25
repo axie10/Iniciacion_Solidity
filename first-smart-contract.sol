540c15f7f35e69e63177ca0c3c9f88bd4b26db1a
axie10 <cryptosushi21@gmail.com>
2026-04-25 11:43:09 +0200

    feat: create functions for int (negative number)

--- first-smart-contract.sol
+++ first-smart-contract.sol
@@ -19,6 +19,7 @@ contract Calculadora {
     // Events
     event AdditionEvent (uint256 num11, uint256 num12, uint256 result);
     event SubstractionEvent (uint256 num11, uint256 num12, uint256 result);
+    event SubstractionNegNumEvent (int256 num11, int256 num12, int256 result);
 
     // External Functions
     function addition (uint256 _num1, uint256 _num2) public returns (uint256 _result){
@@ -31,19 +32,29 @@ contract Calculadora {
         emit SubstractionEvent(_num1, _num2, _result);
     }
 
-    function multiplier (uint256 _num1) public checksNumbers(_num1) {
+    function substractionWithNegNum (int256 _num1, int256 _num2) public returns (int256 _result){
+        _result = substractionLogicToNumNeg(_num1, _num2);
+        emit SubstractionNegNumEvent(_num1, _num2, _result);
+    }
+
+    function multiplier (uint256 _num1) public {
         // Aqui si estamos modificando la variable global del contrato
         resultado = resultado * _num1;
     }
 
-    function multiplier2 (uint256 _num1) public {
+    function multiplier2 (uint256 _num1) public checksNumbers(_num1) {
         // Aqui si estamos modificando la variable global del contrato y añadimos un modificador
         resultado = resultado * _num1;
     }
 
     // Internal Functions (a estas funciones no puede llamar el usuario)
     function substractionLogic (uint256 _num1, uint256 _num2) internal pure returns (uint256 _result){
-        require(_num1 > _num2, "Numero 1 debe ser mayor que numero 2, asi evitamos los negativos");
+        // require(_num1 > _num2, "Numero 1 debe ser mayor que numero 2, asi evitamos los negativos");
+        _result = _num1 - _num2;
+        return _result;
+    }
+
+    function substractionLogicToNumNeg (int256 _num1, int256 _num2) internal pure returns (int256 _result){
         _result = _num1 - _num2;
         return _result;
     }
